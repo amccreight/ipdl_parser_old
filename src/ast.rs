11f7a68eb14cfa6af719ff4960964e5e31b1e31b
Andrew McCreight <continuation@gmail.com>
2016-11-10 07:40:53 -0800

    implement C++ stuff

--- src/ast.rs
+++ src/ast.rs
@@ -5,7 +5,37 @@ pub enum IncludeType {
 }
 
 #[derive(Debug)]
+pub struct QualifiedId {
+    base_id: String,
+    quals: Vec<String>
+}
+
+impl QualifiedId {
+    pub fn new(base: String) -> QualifiedId {
+        QualifiedId { base_id: base, quals: Vec::new() }
+    }
+
+    pub fn qualify(mut self, id: String) -> QualifiedId {
+        self.quals.push(self.base_id);
+        self.base_id = id;
+        self
+    }
+}
+
+#[derive(Debug)]
+pub struct TypeSpec {
+    spec: QualifiedId
+}
+
+impl TypeSpec {
+    pub fn new(spec: QualifiedId) -> TypeSpec {
+        TypeSpec { spec: spec }
+    }
+}
+
+#[derive(Debug)]
 pub enum Node {
     CxxInclude(String),
     Include(IncludeType, String),
+    TypeSpec(TypeSpec),
 }
