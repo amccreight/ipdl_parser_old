7e8dedf23d83aaa9aa678672d67b04508177ddd2
Andrew McCreight <continuation@gmail.com>
2016-11-18 13:53:51 -0800

    add slightly less basic argument parsing for include directories

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,41 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate getopts;
+
 pub mod ast;
 pub mod ipdl;
 pub mod parser;
 pub mod uncommenter;
 
+use getopts::Options;
 use std::path::PathBuf;
 use std::env;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        panic!("Expected two arguments, an include path and a file name.");
+
+    let mut opts = Options::new();
+    opts.optmulti("I", "include",
+                  "Additional directory to search for included protocol specifications",
+                  "DIR");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(f) => { panic!(f.to_string()) },
+    };
+
+    if matches.free.is_empty() {
+        panic!("Expected at least one IPDL file to be specified.");
     }
 
-    let include_dirs = vec![PathBuf::from(&args[1])];
-    let file_name = PathBuf::from(&args[2]);
+    let mut include_dirs = Vec::new();
+    for i in matches.opt_strs("I") {
+        include_dirs.push(PathBuf::from(i))
+    }
+
+    // XXX Handle more than one file being specified.
+    let file_name = PathBuf::from(&matches.free[0]);
+
     println!("Output: {:?}", parser::parse(&include_dirs, &file_name).len());
 }
