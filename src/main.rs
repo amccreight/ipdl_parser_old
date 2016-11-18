43c6305f179e63296eb4860303bd9fea13f4c997
Andrew McCreight <continuation@gmail.com>
2016-11-18 09:32:24 -0800

    add basic specification of include path on the command line

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,23 @@ pub mod uncommenter;
 
 use std::io::prelude::*;
 use std::fs::File;
+use std::path::PathBuf;
 use parser_state::ParserState;
 
 use std::env;
 
 fn main() {
-    let f_name = env::args().nth(1).unwrap();
-    let mut f = File::open(f_name).unwrap();
+    let args : Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        panic!("Expected two arguments, an include path and a file name.");
+    }
+
+    let include_path = PathBuf::from(&args[1]);
+    let mut f = File::open(&args[2]).unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
     s = uncommenter::uncomment(&s);
 
-    let parser_state = ParserState::new(Vec::new());
+    let parser_state = ParserState::new(vec![include_path]);
     println!("Output: {:?}", ipdl::parse_TranslationUnit(&parser_state, &s).unwrap());
 }
