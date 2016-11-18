1fe7aeaf72c3e9ce3b3493d99c3972c9ef48068a
Andrew McCreight <continuation@gmail.com>
2016-11-18 14:10:42 -0800

    add two new more command line options, but just ignore them for now

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,30 @@ use getopts::Options;
 use std::path::PathBuf;
 use std::env;
 
-fn main() {
-    let args : Vec<String> = env::args().collect();
-
+fn get_options_parser() -> Options {
     let mut opts = Options::new();
     opts.optmulti("I", "include",
                   "Additional directory to search for included protocol specifications",
                   "DIR");
+    opts.optopt("d", "outheaders-dir",
+                "Directory into which C++ headers will be generated. \
+                 A protocol Foo in the namespace bar will cause the headers \
+                 dir/bar/Foo.h, dir/bar/FooParent.h, and dir/bar/FooParent.h \
+                 to be generated",
+                "HDR_DIR");
+    opts.optopt("o", "outcpp-dir",
+                "Directory into which C++ sources will be generated \
+                A protocol Foo in the namespace bar will cause the sources \
+                cppdir/FooParent.cpp, cppdir/FooChild.cpp \
+                to be generated",
+                "CPP_DIR");
+    opts
+}
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+
+    let opts = get_options_parser();
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m },
