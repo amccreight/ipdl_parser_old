da9d8ca34c74964da31a22c3d590e422ec48608e
Andrew McCreight <continuation@gmail.com>
2017-02-08 15:47:17 -0800

    Skip expected failing tests using a whitelist instead of a file extension.
    Fixes #21

--- tests/smoke_test.rs
+++ tests/smoke_test.rs
@@ -2,6 +2,8 @@ extern crate ipdl_parser;
 
 use std::fs;
 use std::path::PathBuf;
+use std::collections::HashSet;
+use std::ffi::OsStr;
 
 const BASE_PATH: [&'static str; 2] = [".", "tests"];
 const OK_PATH: &'static str = "ok";
@@ -10,17 +12,64 @@ const ERROR_PATH: &'static str = "error";
 // Tests in error/ are disabled because the given checking is not
 // enabled yet.
 
+const DISABLED_TESTS: &'static [ &'static str ] = &[
+    "compressCtor.ipdl",
+    "compressCtorManagee.ipdl",
+    "conflictProtocolMsg.ipdl",
+    "cyclecheck_Child.ipdl",
+    "cyclecheck_Parent.ipdl",
+    "cyclecheck_Grandchild.ipdl",
+    "intrMessageCompress.ipdl",
+    "managedNoCtor.ipdl",
+    "managedNoDtor.ipdl",
+    "managerNoCtor.ipdl",
+    "managerNoDtor.ipdl",
+    "manageSelfToplevel.ipdl",
+    "messageNoDirection.ipdl",
+    "multimanDupMgrs.ipdl",
+    "multimanDupMgrsMgr.ipdl",
+    "multimanNonexistentMgrs.ipdl",
+    "mutualRecStruct.ipdl",
+    "mutualRecStructUnion.ipdl",
+    "noEmptyToplevel.ipdl",
+    "Nullable2.ipdl",
+    "Nullable.ipdl",
+    "redeclMessage.ipdl",
+    "redeclParamReturn.ipdl",
+    "shmem.ipdl",
+    "structRedecl.ipdl",
+    "structUnknownField.ipdl",
+    "syncMessageCompress.ipdl",
+    "syncParentToChild.ipdl",
+    "tooWeakIntrAsync.ipdl",
+    "tooWeakIntrSync.ipdl",
+    "tooWeakSyncAsync.ipdl",
+    "undeclParamType.ipdl",
+    "undeclProtocol.ipdl",
+    "undeclReturnType.ipdl",
+    "undefMutualRecStruct.ipdl",
+    "undefMutualRecStructUnion.ipdl",
+    "undefMutualRecUnion.ipdl",
+    "undefSelfRecStruct.ipdl",
+    "undefSelfRecUnion.ipdl",
+];
+
 fn test_files(test_file_path: &str, should_pass: bool) {
     let mut path: PathBuf = BASE_PATH.iter().collect();
     path.push(test_file_path);
 
     let include_dirs = vec![path.clone()];
 
+    let mut disabled_tests = HashSet::new();
+    for f in DISABLED_TESTS {
+        disabled_tests.insert(OsStr::new(f));
+    }
+
     let entries = fs::read_dir(&path).expect("Should have the test file directory");
     for entry in entries {
         if let Ok(entry) = entry {
-            if entry.path().extension().unwrap() == "disabled" {
-                assert!(!should_pass, "Expected only error tests to be disabled");
+
+            if !should_pass && disabled_tests.contains(entry.path().file_name().unwrap()) {
                 println!("Skipping {:?}", entry.file_name());
                 // XXX What should happen here is that instead of
                 // continuing, we check to make sure that the test
