3f9697dffd315b565b6a70fd0e0f71c855bcf819
Andrew McCreight <continuation@gmail.com>
2017-02-13 13:54:01 -0800

    start on resolver

--- src/utils.rs
+++ src/utils.rs
@@ -4,18 +4,28 @@
 
 use std::path::{Path, PathBuf};
 
-pub fn resolve_include_path(include_dirs: &Vec<PathBuf>, file_path: &Path) -> Option<PathBuf> {
-    // XXX The Python parser also checks '' for some reason.
-    for d in include_dirs {
-        let mut p = d.clone();
-        p.push(file_path);
+pub struct IncludeResolver {
+    include_dirs: Vec<PathBuf>,
+}
 
-        if p.exists() {
-            if let Ok(pb) = p.canonicalize() {
-                return Some(pb)
-            }
-        }
+impl IncludeResolver {
+    pub fn new(include_dirs: &Vec<PathBuf>) -> IncludeResolver {
+        IncludeResolver { include_dirs: include_dirs }
     }
 
-    return None
+    pub fn resolve(&self, file_path: &Path) -> Option<PathBuf> {
+        // XXX The Python parser also checks '' for some reason.
+        for d in &self.include_dirs {
+            let mut p = d.clone();
+            p.push(file_path);
+
+            if p.exists() {
+                if let Ok(pb) = p.canonicalize() {
+                    return Some(pb)
+                }
+            }
+        }
+
+        return None
+    }
 }
