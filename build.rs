c8eb230821e26cf0a32fd3687fb86166a1c9569d
Kartikaya Gupta <kats@trevize.staktrace.com>
2020-03-23 16:28:51 -0400

    Bug 1624399 - Update lalrpop to latest.

--- build.rs
+++ build.rs
@@ -3,5 +3,8 @@ extern crate lalrpop;
 fn main() {
     println!("cargo:rerun-if-changed=src/ipdl.lalrpop");
 
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .generate_in_source_tree()
+        .process()
+        .unwrap();
 }
