0ac61b95932da5b67e0131bd3918df25ba7a9828
Ray Kraesig <rkraesig@mozilla.com>
2024-03-20 12:12:45 -0700

    Adjust printed format of `Location`
    Change the printed format of Location objects to contain a colon between
    the filename and the line number, as is common across most tooling.

--- src/ast.rs
+++ src/ast.rs
@@ -294,7 +294,7 @@ impl fmt::Display for Location {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{} {}:{}",
+            "{}:{}:{}",
             self.file_name.display(),
             self.lineno,
             self.colno
